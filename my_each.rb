56cd4f285d8ef7c36764be9ca499c790f3d773fb
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-06 20:49:15 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(words) # put argument(s) here
   # code here
   counter = 0
   while counter < words.length
-    yield (words[i])
+    yield (words[counter])
     counter = counter + 1
   end
   words
